d59df1de1d0eaa73e5472a6449bc134a08d22b9c
Antonio Cappiello <antoniocappiello@gmx.com>
2020-04-07 00:10:02 +0200

    script train

--- train.py
+++ train.py
@@ -1,17 +1,7 @@
-from fxtrade.optimize.agents.a2c_batched import A2C
-# from fxtrade.optimize.agents.a2c_batched_small import A2C_small
-from fxtrade.optimize.environment import CandlesBatched, TradingEnvironment
-from fxtrade.data.factory import Downloader
 from fxtrade.optimize.trainer import Trainer
-from torch.utils.data import DataLoader
 import json
-import logging
-
 
 if __name__ == "__main__":
     config = json.load(open("config.json", "r"))
     trainer = Trainer(config)
     trainer.run()
-
-
-
